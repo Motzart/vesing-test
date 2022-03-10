dfea52a0150b1326dc9d0aee25897c11504fbaa7
Alex_Molotsylo <motzart66@gmail.com>
2022-03-10 15:08:14 +0200

    update navbar link

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -11,7 +11,7 @@ export const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Vesting App</a>
+        <a className="navbar-brand" href="/">Vesting App</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
                 aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
