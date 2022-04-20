57a11f423b357bd6e44d1ad19a563888c3db8ce9
Alex_Molotsylo <motzart66@gmail.com>
2022-04-20 13:51:37 +0300

    add lockup logic

---
+++ src/hooks/useLockup.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getLockupsPaged } from '~services/near';
+
+export interface UserLockupsView {
+  account_id: string,
+  schedule: [],
+  claimed_balance: string,
+  termination_config: null,
+  timestamp: number,
+  total_balance: string,
+  unclaimed_balance: string,
+}
+
+export const useLockup = () => {
+  const [userLockupPage, setLockupPage] = useState<UserLockupsView>();
+
+  useEffect(() => {
+    getLockupsPaged()
+      .then((data: any) => {
+        console.log('-------', data)
+        return setLockupPage(data);
+      })
+      .catch(() => setLockupPage(null));
+  }, []);
+
+  return userLockupPage;
+};
---
+++ src/utils/numbers.ts
@@ -0,0 +1,49 @@
+export const separation = (num: number | string) => {
+  if (typeof(num) === 'string') {
+     return Number(num).toLocaleString('ru')
+  }
+  return num.toLocaleString('ru');
+}
+
+export const toReadableNumber = (
+  decimals: number,
+  number: string = '0'
+): string => {
+  if (!decimals) return number;
+
+  const wholeStr = number.substring(0, number.length - decimals) || '0';
+  const fractionStr = number
+    .substring(number.length - decimals)
+    .padStart(decimals, '0')
+    .substring(0, decimals);
+
+  return `${wholeStr}.${fractionStr}`.replace(/\.?0+$/, '');
+};
+
+export const toPrecision = (
+  number: string,
+  precision: number,
+  withCommas: boolean = false,
+  atLeastOne: boolean = true
+): string => {
+  const [whole, decimal = ''] = number.split('.');
+
+  let str = `${withCommas ? formatWithCommas(whole) : whole}.${decimal.slice(
+    0,
+    precision
+  )}`.replace(/\.$/, '');
+  if (atLeastOne && Number(str) === 0 && str.length > 1) {
+    var n = str.lastIndexOf('0');
+    str = str.slice(0, n) + str.slice(n).replace('0', '1');
+  }
+
+  return str;
+};
+
+export function formatWithCommas(value: string): string {
+  const pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(value)) {
+    value = value.replace(pattern, '$1,$2');
+  }
+  return value;
+}
